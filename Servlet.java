d7af7c4f649b92891b4c4afc6719c13966f9d69d
ChaserNight <jiajundai68@gmail.com>
2021-12-06 18:27:12 +0800

    初始Servlet.java

---
+++ Servlet.java
@@ -0,0 +1,28 @@
+package com.cqu.servlet; //注意更改包
+
+// import com.cqu.service.HelloService;
+// import com.cqu.service.impl.HelloServiceImpl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.annotation.WebServlet;
+
+
+public class Servlet extends HttpServlet {
+    static Map<url, Object> map;
+
+    // method    get post
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // System.out("----------");
+
+        // 处理逻辑   class    method
+        // servlet1
+    }
+
+    
+}
